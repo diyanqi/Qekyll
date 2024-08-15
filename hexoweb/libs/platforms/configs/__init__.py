01d3314c3cf114aaddd7c39c7dcdafbe6884791d
Dignite <60802943+diyanqi@users.noreply.github.com>
2024-08-15 18:12:07 +0800

    Update __init__.py

--- hexoweb/libs/platforms/configs/__init__.py
+++ hexoweb/libs/platforms/configs/__init__.py
@@ -1,5 +1,6 @@
 from . import hexo
 from . import hugo
+from . import jekyll
 from . import valaxy
 from . import vuepress
 from . import vitepress
@@ -7,6 +8,7 @@ from . import vitepress
 _all_configs = {
     hexo.config["name"]: hexo.config,
     hugo.config["name"]: hugo.config,
+    jekyll.config["name"]: jekyll.config,
     valaxy.config["name"]: valaxy.config,
     vuepress.config["name"]: vuepress.config,
     vitepress.config["name"]: vitepress.config
